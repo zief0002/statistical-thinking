e9edbe51995f75de6502f40d5de03f00551251eb
zief0002 <zief0002@umn.edu>
2025-01-08 14:14:58 -0600

    Updated book

---
+++ assets/statistical-thinking.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+// Function to toggle the visibility of solutions in exercises
+function toggle_visibility(id) {
+   var e = document.getElementById(id);
+   if(e.style.display == 'block')
+      e.style.display = 'none';
+   else
+      e.style.display = 'block';
+}
+</script>
